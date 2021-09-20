9e868f9a3289e6c81397e62df1d72b1cad18d603
Rajanikanth <rajanikanthkore2@gmail.com>
2021-09-20 16:20:56 +0530

    Centre Service Controller Updated

--- src/main/java/com/htc/centreservice/controller/CentreController.java
+++ src/main/java/com/htc/centreservice/controller/CentreController.java
@@ -1,5 +1,7 @@
 package com.htc.centreservice.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import com.htc.centreservice.persistance.ICentre;
 import com.htc.centreservices.entity.Centre;
@@ -16,21 +19,20 @@ import com.htc.centreservices.entity.Centre;
 public class CentreController {
 	
 	@Autowired
-	private ICentre repo;
-
-	@GetMapping("/centres/{centreId}")
-	Centre getPatient(@PathVariable Long centreId)
-	{
-		Centre cen=repo.getById(centreId).get();
-		return cen;
-		
+	private Centre icentre;
+	
+	@GetMapping("/centre/{centre_id}")
+	public Centre getCentrebyId(@PathVariable Long centre_id) {
+		return icentre.findById(centre_id).get();
 	}
-
-
-
-	@PostMapping("/centres")
-	Centre createPatient(@RequestBody Centre cen)
-	{
-		return cen;
+	
+	@PostMapping("/create/centre")
+	public Centre createCentre(@RequestBody Centre centre) {
+		return icentre.save(centre);
+	}
+	
+	@GetMapping("/centre/{pincode}")
+	public List<Centre> getCentrebypinCode(@PathVariable Long pincode) {
+		return icentre.findByPin_code(pincode);
+	}
 }
-	}
--- src/main/java/com/htc/centreservice/persistance/ICentre.java
+++ src/main/java/com/htc/centreservice/persistance/ICentre.java
@@ -1,13 +1,21 @@
 package com.htc.centreservice.persistance;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.htc.centreservices.entity.Centre;
 
 
 
+
+
 @Repository
-public interface ICentre  extends JpaRepository<Centre,Long>{
+public interface ICentre extends  JpaRepository<Centre,Long> {
+	
+	@Query(value="SELECT centre_id,centre_address,pin_code FROM Centre_Service WHERE pin_code=?1",nativeQuery = true)
+	public List<Centre> findByPin_code(Long pin_code);
 
 }
